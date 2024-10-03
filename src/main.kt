1fcc93039820b2a8fbfe24794827edd8f81caea8
Sinclair-boop <ngapoutfotso@gmail.com>
2024-10-03 06:49:32 -0700

    Projet1

---
+++ src/main.kt
@@ -0,0 +1,8 @@
+fun main(args : Array<String>){
+    var text : String? = null
+    text+=" Kotlin"
+    text+=" World"
+    println("Hello ${text}")
+/** val est une constante*/
+    val text1 = " Kotlin"
+}
